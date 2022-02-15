101da08fa9f704d7894553ed46f4d810a5a8a26d
Andre Jacobs <andrejacobs@users.noreply.github.com>
2022-02-15 13:34:27 +0000

    Added a new blank project that targets iOS, macOS using SwiftUI and also added a macOS Command line app.

---
+++ FlyingFoxSandbox/FlyingFoxSandboxCLI/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  FlyingFoxSandboxCLI
+//
+//  Created by André Jacobs on 15/02/2022.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ FlyingFoxSandbox/Shared/FlyingFoxSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyingFoxSandboxApp.swift
+//  Shared
+//
+//  Created by André Jacobs on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlyingFoxSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
