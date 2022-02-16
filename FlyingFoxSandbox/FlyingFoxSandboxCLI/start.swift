7c8e217157347390dbcb431fe2e031066b78272c
Andre Jacobs <andrejacobs@users.noreply.github.com>
2022-02-16 09:24:11 +0000

    wip. Ok it turns out you can do async stuff in CLI, but you need @main. Ironically you can't have this code inside main.swift.

---
+++ FlyingFoxSandbox/FlyingFoxSandboxCLI/start.swift
@@ -0,0 +1,24 @@
+//
+//  start.swift [aka main.swift but not main.swift]
+//  FlyingFoxSandboxCLI
+//
+//  Created by André Jacobs on 15/02/2022.
+//
+
+import Foundation
+import FlyingFox
+
+@main
+struct UserFetcher {
+    static func main() async throws {
+        print("Hello")
+    }
+}
+
+//let server = HTTPServer(port: 8080)
+//let task = Task { try await server.start() }
+//
+//sleep(10)
+//
+//task.cancel()
+//
